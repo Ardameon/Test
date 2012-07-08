0c78762e48d5449b7bf23e18cbfa9c2cb36bd39d
Mishka <helldiver2@yandex.ru>
2012-07-09 02:04:24 +0700

    main.c changed

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <iomanip>
 #include "header.h"
 
 using namespace std;
 
 int main(int argc, const char *argv[])
 {
+	Number n = 25.0;
+	cout << "Number = " << hex << n << endl;
+	cout << "Number in hexogonal = " << hex << 25 << endl;
 	return 0;
 }
